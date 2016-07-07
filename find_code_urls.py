1d97f2ae04a72c2b9307f2fdd3e6ee537e6698e2
John Bradley <johnbradley2008@gmail.com>
2016-07-07 14:58:33 -0400

    saving to file including title

--- find_code_urls.py
+++ find_code_urls.py
@@ -22,7 +22,6 @@ DOMAIN_PARTS = [
     "identifiers.org",
 ]
 DATABASE = "pubmed"
-EMAIL = "john.bradley@duke.edu"
 # maximum records that can be returned
 # expected to find much less than this...
 NCBI_RETMAX = 100000
@@ -42,9 +41,11 @@ def is_url_valid(url):
     try:
         r = requests.get(format_url(url))
         result = r.ok
+        if not result:
+            print("Unable to connect to URL: {} result {}".format(url, r.status_code))
         return result
     except ConnectionError as err:
-        print("BAD URL: {}".format(url))
+        print("Unable to connect to URL: {}".format(url))
         return False
 
 
@@ -57,42 +58,55 @@ def format_url(url):
     return url
 
 
-def find_urls_for_domain_parts(outfilename, domain_parts, email):
-    Entrez.email = EMAIL
+def search_for_articles(domain_parts):
     search_terms = " or ".join(domain_parts)
     print("Running esearch.")
     handle = Entrez.esearch(db=DATABASE, retmax=NCBI_RETMAX, term=search_terms)
     record = Entrez.read(handle)
     handle.close()
-    id_to_urls = {}
+    count = record['Count']
+    print("Found", count, 'articles.')
+    return count, record['IdList']
+
+
+def parse_abstract_data(record):
+    pmid = str(record['MedlineCitation']['PMID'])
+    article = record['MedlineCitation']['Article']
+    title = article['ArticleTitle'].encode('latin-1', 'replace')
+    abstract = None
+    if article.get('Abstract'):
+        so = article['Abstract']['AbstractText'][0]
+        abstract = so.encode('latin-1', 'replace')
+    return pmid, title, abstract
+
+
+def find_urls_for_domain_parts(outfilename, domain_parts, email):
+    Entrez.email = email
+    num_found_articles, article_id_list = search_for_articles(domain_parts)
     cnt = 0
-    print("Found", record['Count'], 'articles.')
-    ids = ",".join(record['IdList'])
+    ids = ",".join(article_id_list)
     num_urls = 0
     with open(outfilename, 'w') as outfile:
-        handle2 = Entrez.efetch(db=DATABASE, id=ids, retmode="xml")
-        records = Entrez.parse(handle2)
+        handle = Entrez.efetch(db=DATABASE, id=ids, retmode="xml")
+        records = Entrez.parse(handle)
         for record in records:
-            id = str(record['MedlineCitation']['PMID'])
-            article = record['MedlineCitation']['Article']
-            title = article['ArticleTitle'].encode('latin-1', 'replace')
-            if article.get('Abstract'):
+            pmid, title, abstract = parse_abstract_data(record)
+            if abstract:
                 try:
-                    so = article['Abstract']['AbstractText'][0]
-                    abstract_str = so.encode('latin-1', 'replace')
-                    text = abstract_str
-                    tokens = nltk.word_tokenize(text)
+                    tokens = nltk.word_tokenize(abstract)
                     urls_in_abstract = [format_url(token) for token in tokens if is_source_code_url(domain_parts, token)]
                     for url in urls_in_abstract:
                         num_urls += 1
-                        outfile.write("{}\t{}\t{}\n".format(id, title, url))
+                        outfile.write("{}\t{}\t{}\n".format(pmid, title, url))
                 except UnicodeDecodeError as err:
-                    print(err)
+                    print("Failed to parse abstract for {}: {}".format(pmid, err))
             cnt += 1
             if cnt % PROG_CNT == 0:
-                print(cnt)
-        handle2.close()
+                sys.stdout.write('\rProcessed {} of {}'.format(cnt, num_found_articles))
+        handle.close()
     print("Urls in Articles=", num_urls)
 
-
-find_urls_for_domain_parts(sys.argv[1], DOMAIN_PARTS, EMAIL)
+if len(sys.argv) != 3:
+    print("Usage: python find_code_urls.py <OUTPUT_FILENAME> <YOUR_EMAIL>")
+else:
+    find_urls_for_domain_parts(sys.argv[1], DOMAIN_PARTS, sys.argv[2])
